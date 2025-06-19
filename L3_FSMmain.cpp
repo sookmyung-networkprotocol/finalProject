6fc8f4a48786e8f533a1455d725db03dd4e5c830
yoon <sin020511@gmail.com>
2025-06-20 04:44:45 +0900

    [TEST] 순서

--- L3_FSMmain.cpp
+++ L3_FSMmain.cpp
@@ -795,10 +795,11 @@ void L3_FSMrun(void)
 
                 char ackMsg[4];
                 sprintf(ackMsg, "%d", voteTo);
+
+                L3_event_clearEventFlag(L3_event_msgRcvd);
                 L3_LLI_dataReqFunc((uint8_t*)ackMsg, strlen(ackMsg), 1);
                 pc.printf("\r\n📤 [게스트] %d번에게 투표 결과 전송 완료\n", voteTo);
-                L3_event_clearEventFlag(L3_event_msgRcvd);
-                wait_ms(500);
+
                 change_state = 2;
             }
 
